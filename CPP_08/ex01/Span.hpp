ae95db5f5ae41e82330ed422b5e0f2b6f7b8348a
maria <wrickard@student.21-school.ru>
2022-05-13 23:06:25 +0300

    night work

---
+++ CPP_08/ex01/Span.hpp
@@ -0,0 +1,53 @@
+#ifndef SPAN_HPP
+#define SPAN_HPP
+#include <iostream>
+#include <algorithm>
+
+/*Мы делаем класс для хранения n кол-ва элементов
+(unsigned int).
+*addNumber функция, которая хранит 1 число
+*shortestSpan означает разницу между 
+минимальным значением внутреннего элемента 
+и следующим минимальным значением, а 
+*longestSpan означает разницу между минимальным 
+значением и максимальным значением
+*rangeAddNumber - метол для добавления
+диапазона итераторов в вектор
+*/
+
+class Span{
+	private:
+		std::vector<int>	_value;
+		unsigned int		_maxElementsValue;
+	public:
+		Span();
+		~Span();
+		Span(Span const &src);
+		Span &operator=(Span const &src);
+		Span(unsigned int n);
+		void addNumber(int num);
+		void addRange(std::vector<int>::iterator begin, std::vector<int>::iterator end);
+		int shortesSpan();
+		int longestSpan();
+		class vectorIsFull : public std::exception{
+			public :
+				virtual const char* what() const throw();
+		};
+		class vectorIsLessThanTwo : public std::exception{
+			public :
+				virtual const char* what() const throw();
+		};
+		
+};
+
+#endif
+
+/*
+функция с именем addRange класса 
+Span должна иметь возможность работать 
+с любым Container, поэтому ее следует 
+определить как шаблон функции. В настоящее 
+время необходимо иметь возможность 
+получать итератор и указатель, поэтому 
+трудно решить, как определить имя типа.
+*/
